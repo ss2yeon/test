105eeb6b3405913d9a0c6c2b88223efbfc85446f
ss2yeon <soyon6743@spreatics.com>
2023-02-03 16:16:05 +0900

    study: 배열 선언

--- 0203_lecture/0203_lecture/01_array.cpp
+++ 0203_lecture/0203_lecture/01_array.cpp
@@ -1,4 +1,16 @@
 #include <iostream>
 int main() {
 
+	std::string friends[2] = { "È«±æµ¿", "¼ºÃáÇâ" };
+	std::cout << friends[0];
+
+	double nums[3] = { 1.5, 2.5, 3.5 };
+
+	std::string fruit[2][3][2] = {
+		{ { "",""} , {"",""}, {"",""} },
+		{ { "",""} , {"",""}, {"",""} }
+	};
+
+	// friends = { "È«±æµ¿", "¼ºÃáÇâ" }
+	return 0;
 }
